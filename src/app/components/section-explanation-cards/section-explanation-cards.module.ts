01ca8d8d3d82c01ec9451b6eca04774e33555d9a
Alejandro <alexibape@hotmail.com>
2020-04-19 21:39:11 +0200

    aibanepe: why OK

--- src/app/components/section-explanation-cards/section-explanation-cards.module.ts
+++ src/app/components/section-explanation-cards/section-explanation-cards.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionExplanationCardsComponent } from './section-explanation-cards.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [SectionExplanationCardsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [SectionExplanationCardsComponent]
 })
--- src/app/components/section-explanation/section-explanation.module.ts
+++ src/app/components/section-explanation/section-explanation.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionExplanationComponent } from './section-explanation.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [SectionExplanationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
 exports: [SectionExplanationComponent],
 
